149f2db5001b00c47b01ed864c15a91d4c04e647
GuuscoNL <99217547+GuuscoNL@users.noreply.github.com>
2022-06-14 23:45:46 +0200

    Make ping silent

--- cogs/ping.py
+++ cogs/ping.py
@@ -20,14 +20,14 @@ class ping(commands.Cog):
 
     @app_commands.checks.has_any_role(ADMIN_ROLE_ID) # Check if the author has the admin role. If not go to @ping.error
     async def ping(self, interaction : discord.Interaction) -> None:
-        await interaction.response.send_message(f"pong ({str(self.bot.latency*1000)[0:6]} ms)")
+        await interaction.response.send_message(f"pong ({str(self.bot.latency*1000)[0:6]} ms)", ephemeral=True)
         print(f"[{await self.current_time()}] {interaction.user.name} pinged the bot")
         
     @ping.error
     async def permission(self, interaction : discord.Interaction, error : app_commands.AppCommandError) -> None:
         if isinstance(error, app_commands.MissingAnyRole): # Check if the error is because of an missing role
             if interaction.user.id == 397046303378505729:# Check if the author is me (GuuscoNL)
-                await interaction.response.send_message(f"pong ({str(self.bot.latency*1000)[0:6]} ms)")
+                await interaction.response.send_message(f"pong ({str(self.bot.latency*1000)[0:6]} ms)", ephemeral=True)
                 print(f"[{await self.current_time()}] {interaction.user.name} pinged the bot")
             else:
                 await interaction.response.send_message("You do not have the permission!", ephemeral=True)
