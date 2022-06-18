820f3b267c269fe53029c9e2ea8e0f58fbdcc19a
GuuscoNL <99217547+GuuscoNL@users.noreply.github.com>
2022-06-18 20:50:13 +0200

    Make /stardate Ephemeral

--- cogs/stardate.py
+++ cogs/stardate.py
@@ -21,19 +21,19 @@ class stardate(commands.Cog):
       b = 2323 # reference earthdate
       c = 0 # reference stardate
 
-      t = datetime.datetime.now().timetuple()
+      t = datetime.datetime.now().timetuple() # Get current time
       year = t.tm_year + 360 # convert current year to correct year (2022 = 2382, so + 360)
 
-      if year % 400 == 0 or (year % 4 == 0 and year % 100 != 0):
+      if year % 400 == 0 or (year % 4 == 0 and year % 100 != 0): #check if leap year
             n = 366
       else:
             n = 365
 
       result = format(((c + (1000*(year - b))) +
                            ((1000/((n)*1440.0))*(((
-                                 t.tm_yday - 1.0)*1440.0) +
-                              (t.tm_hour*60.0) + t.tm_min))), '.2f')
-      await interaction.response.send_message(f"Current stardate is {result}")
+                           t.tm_yday - 1.0)*1440.0) +
+                           (t.tm_hour*60.0) + t.tm_min))), '.2f')
+      await interaction.response.send_message(f"Current stardate is {result}", ephemeral=True)
 
 
 async def setup(bot : commands.Bot) -> None:
