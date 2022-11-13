675997d4710cab8ba1a252aebe65277626b5e8a7
GuuscoNL <99217547+GuuscoNL@users.noreply.github.com>
2022-11-13 01:08:12 +0100

    Add logging to /json

--- cogs/json.py
+++ cogs/json.py
@@ -5,6 +5,7 @@ from discord.ext import commands
 from dotenv import load_dotenv
 import os
 import json
+import logging
 
 load_dotenv() # load the variables needed from the .env file
 SERVER_ID = os.getenv('SERVER_ID')
@@ -13,6 +14,18 @@ ADMIN_ROLE_ID = int(os.getenv('ADMIN_ROLE_ID'))
 from pathlib import Path
 path_dir = Path(__file__).parent.parent.resolve()
 path_json = path_dir / "data.JSON"
+
+#logging
+logger = logging.getLogger("json")
+
+formatter = logging.Formatter("[%(asctime)s] %(levelname)s:%(name)s: %(message)s",
+                              "%Y-%m-%d %H:%M:%S")
+
+file_handler = logging.FileHandler("main.log")
+file_handler.setFormatter(formatter)
+
+logger.addHandler(file_handler)
+logger.setLevel(logging.INFO)
 
 class command_json(commands.Cog):
 
@@ -25,29 +38,46 @@ class command_json(commands.Cog):
 
    @app_commands.checks.has_any_role(ADMIN_ROLE_ID) # Check if the author has the admin role. If not go to @json.error
    async def command_json(self, interaction : discord.Interaction) -> None:
+      await set_debug_level()
       with path_json.open(mode="r") as file:
          json_data = json.loads(file.read())   
       await interaction.response.send_message(f"```{json_data}```", ephemeral=True)
-      print(f"[{await self.current_time()}] {interaction.user.name} looked at the JSON file")
+      logger.info(f"{interaction.user.name} used the `/json` command")
       
    @command_json.error
    async def permission(self, interaction : discord.Interaction, error : app_commands.AppCommandError) -> None:
       if isinstance(error, app_commands.MissingAnyRole): # Check if the error is because of an missing role
+         await set_debug_level()
          if interaction.user.id == 397046303378505729:# Check if the author is me (GuuscoNL)
                with path_json.open(mode="r") as file:
                   json_data = json.loads(file.read())
                await interaction.response.send_message(f"```{json_data}```", ephemeral=True)
-               print(f"[{await self.current_time()}] {interaction.user.name} looked at the JSON file")
+               logger.info(f"{interaction.user.name} used the `/json` command")
          else:
                await interaction.response.send_message("You do not have the permission!", ephemeral=True)
-               print(f"[{await self.current_time()}] {interaction.user.name} tried to use the `/json` command")
-
-   async def current_time(self): # Get current time
-      now = datetime.datetime.utcnow()
-      return now.strftime("%d/%m/%Y %H:%M:%S UTC")
+               logger.info(f"{interaction.user.name} tried to use the `/json` command")
 
 async def setup(bot : commands.Bot) -> None:
    await bot.add_cog(
       command_json(bot),
       guilds = [discord.Object(id = SERVER_ID)]
    )
+   
+async def set_debug_level():
+    with path_json.open() as file:
+        json_data = json.loads(file.read())
+        debuglevel = json_data["debuglevel"]
+    
+    if debuglevel == "DEBUG":
+        logger.setLevel(logging.DEBUG)
+    elif debuglevel == "INFO":
+        logger.setLevel(logging.INFO)
+    elif debuglevel == "WARNING":
+        logger.setLevel(logging.WARNING)
+    elif debuglevel == "ERROR":
+        logger.setLevel(logging.ERROR)
+    elif debuglevel == "CRITICAL":
+        logger.setLevel(logging.CRITICAL)
+    else:
+        print("no debug level set")
+        logger.setLevel(logging.NOTSET)
