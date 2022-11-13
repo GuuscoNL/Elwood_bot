266574190fd8d396b17b09e7db9a6ee43878ecc6
GuuscoNL <99217547+GuuscoNL@users.noreply.github.com>
2022-11-13 01:20:29 +0100

    Add logging to /sleep_mode

--- cogs/sleep_mode.py
+++ cogs/sleep_mode.py
@@ -5,6 +5,7 @@ from dotenv import load_dotenv
 import datetime
 import os
 import json
+import logging
 
 load_dotenv() # load the variables needed from the .env file
 SERVER_ID =os.getenv('SERVER_ID')
@@ -13,6 +14,18 @@ ADMIN_ROLE_ID = int(os.getenv('ADMIN_ROLE_ID'))
 from pathlib import Path
 path_dir = Path(__file__).parent.parent.resolve()
 path_json = path_dir / "data.JSON"
+
+#logging
+logger = logging.getLogger("sleep_mode")
+
+formatter = logging.Formatter("[%(asctime)s] %(levelname)s:%(name)s: %(message)s",
+                              "%Y-%m-%d %H:%M:%S")
+
+file_handler = logging.FileHandler("main.log")
+file_handler.setFormatter(formatter)
+
+logger.addHandler(file_handler)
+logger.setLevel(logging.INFO)
 
 class sleep_mode(commands.Cog):
 
@@ -25,22 +38,24 @@ class sleep_mode(commands.Cog):
 
    @app_commands.checks.has_any_role(ADMIN_ROLE_ID) # Check if the author has the admin role. If not go to @toggle_server.error
    async def debug(self, interaction : discord.Interaction) -> None:
+      await set_debug_level()
       mode = await self.update_json()
       msg = f"Mode is set to to {mode}"
       await interaction.response.send_message(msg, ephemeral=True)
-      print(f"[{await self.current_time()}] {interaction.user.name} changed mode to {mode}")
+      logger.info(f"{interaction.user.name} changed mode to {mode}")
       
    @debug.error
    async def permission(self, interaction : discord.Interaction, error : app_commands.AppCommandError) -> None:
       if isinstance(error, app_commands.MissingAnyRole): # Check if the error is because of an missing role
+         await set_debug_level()
          if interaction.user.id == 397046303378505729:# Check if the author is me (GuuscoNL)
             mode = await self.update_json()
             msg = f"Mode is set to {mode}"
             await interaction.response.send_message(msg, ephemeral=True)
-            print(f"[{await self.current_time()}] {interaction.user.name} changed mode to {mode}")
+            logger.info(f"{interaction.user.name} changed mode to {mode}")
          else:
             await interaction.response.send_message("You do not have the permission!", ephemeral=True)
-            print(f"[{await self.current_time()}] {interaction.user.name} tried to use the `/sleep_mode` command") # Log it
+            logger.info(f"{interaction.user.name} tried to use the `/sleep_mode` command") # Log it
    
    async def update_json(self):
       with path_json.open(mode="r+") as file:
@@ -54,13 +69,28 @@ class sleep_mode(commands.Cog):
          file.truncate(0)
          file.write(temp)
       return json_data["mode"]
-   
-   async def current_time(self): # Get current time
-      now = datetime.datetime.utcnow()
-      return now.strftime("%d/%m/%Y %H:%M:%S UTC")
 
 async def setup(bot : commands.Bot) -> None:
    await bot.add_cog(
       sleep_mode(bot),
       guilds = [discord.Object(id = SERVER_ID)]
    )
+
+async def set_debug_level():
+    with path_json.open() as file:
+        json_data = json.loads(file.read())
+        debuglevel = json_data["debuglevel"]
+    
+    if debuglevel == "DEBUG":
+        logger.setLevel(logging.DEBUG)
+    elif debuglevel == "INFO":
+        logger.setLevel(logging.INFO)
+    elif debuglevel == "WARNING":
+        logger.setLevel(logging.WARNING)
+    elif debuglevel == "ERROR":
+        logger.setLevel(logging.ERROR)
+    elif debuglevel == "CRITICAL":
+        logger.setLevel(logging.CRITICAL)
+    else:
+        print("no debug level set")
+        logger.setLevel(logging.NOTSET)
